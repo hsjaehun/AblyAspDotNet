8664a8bcf014f01c46b1f220d27918720f3fd9a4
hsjaehun <hsjaehun@gmail.com>
2021-07-07 14:14:37 -0400

    First Commit

---
+++ Controllers/PublishController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IO.Ably;
+
+namespace AblyAspDotNet.Controllers
+{
+    public class PublishController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult castVote(string button)
+        {
+            var realtime = new AblyRealtime("0BaVrg.FPCCkg:Bi_JOu2bl5MuCDZ5");
+            var channel = realtime.Channels.Get("voting-channel");
+            channel.Publish("vote", button);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
